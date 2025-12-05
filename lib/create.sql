412ad65c9e7692adf4a9640ef5063df94dfad6e9
Christine Mumbi <christinemumbi286@gmail.com>
2025-12-05 11:58:37 +0300

    Used sqlite3 to build database

---
+++ lib/create.sql
@@ -0,0 +1,1 @@
+CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT , age INTEGER, sex TEXT NOT NULL, color TEXT NOT NULL, temperament TEXT NOT NULL, alive BOOLEAN);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'brown', 'calm', 1),
+('Rowdy', 15, 'M', 'black', 'aggressive', 1),
+('Tabitha', 10, 'F', 'white', 'friendly', 1),
+('Sergeant Brown', 18, 'M', 'brown', 'strict', 1),
+('Melissa', 12, 'F', 'gray', 'docile', 1),
+('Grinch', 7, 'M', 'green', 'grumpy', 1),
+('Wendy', 5, 'F', 'blonde', 'playful', 1),
+(NULL, 3, 'M', 'black', 'mysterious', 0);
